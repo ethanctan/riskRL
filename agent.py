a522b7486239c31e201312753d24d5b6d49306bd
Ethan Tan <ectan@college.harvard.edu>
2023-12-03 22:31:32 -0500

    more agent edits

--- agent.py
+++ agent.py
@@ -8,13 +8,14 @@ class Agent:
         self.nNodes = len(self.game.map.nodes)
         self.nPlayers = len(self.game.players)
         self.nStates = ((NODELIMIT + 1) * (self.nPlayers + 1)) ** self.nNodes
-        self.nActions = 
+        self.nActions = 2 * len(self.game.map.edges) + 1 # Each edge (i, j) has 2 actions: i -> j and j -> i. also, the pass action.
         self.pi = pi # NOTE: Initialize random policy in the training script
     
     def updateGameState(self, updatedGame: Game):
         self.game = updatedGame
 
-    def approximateP()
+    def approximateP(self):
+        pass
         # Naive approach: For each state, look at which states it transitioned to, then update the probabilities
 
 # TODO: Find a way to index the states and actions
